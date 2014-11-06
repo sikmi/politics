5b08fad257467aa182062cc87461ca9775e246c2
Takuya Kuruma <tk.kuruma@gmail.com>
2014-11-06 21:26:16 +0900

    お問い合わせページも作成

---
+++ app/controllers/lp/contacts_controller.rb
@@ -0,0 +1,2 @@
+class Lp::ContactsController < Lp::ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
 
   root "lp/welcome#index"
 
+  scope "/lp", module: "lp", as: :lp do
+    resources :contacts, only: [:new,:show]
+  end
+
 end
