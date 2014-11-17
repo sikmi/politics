a5946b90ef089d24d4097142a96519d8765dc5d2
root <root@www3183gj.sakura.ne.jp>
2014-11-18 07:24:19 +0900

    proのメールもサクラにした

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => "seijihashin.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "skmi.sakura.ne.jp",
+    :port => 587,
+    :domain => 'sikmi.com',
+    :user_name => "kuruma@sikmi.com",
+    :password => "kuruma123",
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+  }
+
+
 end
